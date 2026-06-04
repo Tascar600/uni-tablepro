f07be85a9324f7a546054201b8acf451c7c694c9
I'm_Tascar <punhamasiwa@gmail.com>
2026-06-04 02:51:01 +0200

    Fix: add missing openModal function to app.js so edit modals work

--- timetable_system/static/app.js
+++ timetable_system/static/app.js
@@ -204,6 +204,11 @@ function forceMove() {
     document.getElementById('conflictModal').classList.remove('show');
 }
 
+function openModal(id) {
+    const el = document.getElementById(id);
+    if (el) el.classList.add('show');
+}
+
 function closeModal(id) {
     const el = document.getElementById(id);
     if (el) el.classList.remove('show');
